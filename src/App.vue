f422fbfc11140431ab7729be345de3c9f7a75d97
khaitrinh1207 <khaitrinh1207@gmail.com>
2021-06-13 22:18:08 +0700

    tao project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+  <navbar></navbar>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import Navbar from "@/components/header/navbar.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {Navbar}
 }
 </script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/header/navbar.vue
@@ -0,0 +1,15 @@
+<template src="./navbar.html">
+</template>
+
+<script>
+export default {
+  name: "navbar",
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+}
+</script>
+
+<style src="./navbar.scss" lang="scss" scoped>
+</style>
